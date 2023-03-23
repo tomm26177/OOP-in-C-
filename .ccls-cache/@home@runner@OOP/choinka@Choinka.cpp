8d737f240ee31ba966b3bc7195b8924c42adc7d1
Tom -_- <>
2023-03-23 07:46:44 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@OOP/choinka@Choinka.cpp
@@ -0,0 +1,78 @@
+#include <iostream>>
+#include <stdlib.h>
+
+class Choinka{
+
+
+private:
+    int height;
+    int weight;
+    char symbol;
+
+
+
+Choinka(){
+  draw();
+}
+
+public:
+void draw(){
+  
+  int amoutOfSpaces = 0;
+  int amoutOfStars = 0;
+  
+
+  for(int i=1; i<height*2;i=i+2){
+
+    amoutOfStars=i;
+    amoutOfSpaces=height*2-i;
+drawSegment( amoutOfStars,  amoutOfSpaces);
+    
+  
+  
+  }
+}
+
+public:
+void drawSegment(int amoutOfStars, int amoutOfSpaces){
+   for(int i=0;i<amoutOfSpaces/2;i++){
+    std::cout<<" ";
+  }
+
+  
+  for(int i=0;i<amoutOfStars;i++){
+    std::cout<<symbol;
+  }
+
+
+   for(int i=0;i<amoutOfSpaces/2;i++){
+    std::cout<<" ";
+  }
+ 
+  std::cout<<"\n";
+}
+
+
+public:
+int getHeight(){
+  return height;
+}
+
+public:
+void setHeight(int h){
+   height = h;
+}
+
+
+public:
+char getSymbol(){
+  return symbol;
+}
+
+public:
+void setSymbol(char s){
+   symbol= s;
+}
+
+
+};
---
+++ choinka/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Choinka.cpp"
+
+int main() {
+  
+  
+Choinka choinka;
+choinka.setHeight(5);
+choinka.setSymbol('^');
+  
+  
+  std::cout << "\033[1;31mred\033[0m\n";
+  
+};
