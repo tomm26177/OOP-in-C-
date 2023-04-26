3d3cef690e35431383650f55277a564a73b266c2
Tomasz Kamiński <104736559+tomm26177@users.noreply.github.com>
2023-04-26 20:50:41 +0200

    Update main.cpp

--- kodDoSformatowania/main.cpp
+++ kodDoSformatowania/main.cpp
@@ -2,332 +2,192 @@
 //
 
 #include <iostream>
-#include <conio.h>
-#include <Windows.h>
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
-#include <string>  
 #include <fstream>
+#include <string>
 #include <vector>
-#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <cctype>
+#include <locale>
 #include <cassert>
-#include <locale.h>
 
 using namespace std;
 
-struct Gracz {
+class Player {
+public:
     string imie;
-    int kasa = 0;
-    int portfel = 0; //kasa z wygranych rund
+    int portfel;
+    int kasa;
 };
 
-struct Gracz gracze[3];
-
-int kolejka = 0;
-
-int Kolo[] = { -1, 0, 100, 200, 100, 200, 100, 200, 500, 500, 1000, 1000, 1500, 2000, 3000, 5000 };
-// -1 bankrut
-// 0 strata kolejki
-
-
-void textPlayers()
-{
-    int i;
-    std::cout << "\n";
-    for (i = 0; i < 3; i++) {
-        if (i == kolejka) {
-            cout << "\033[1;34m";
-        }
-        cout << gracze[i].imie << "\t" << gracze[i].kasa << "\n";
-        cout << "\033[0m";
-
-    }
-    cout << "\n";
-}
-
-int jestSamogloska(char c)
-{
-    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y' ||
-        c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'Y')
-        return 1;
-    else
-        return 0;
-
-}
-
-char WczytajZnak()
-{
-    string letter;
-    cin >> letter;
-    
-    while (letter.length() != 1)
-    {
-        cout << endl << "Type a single letter and press <enter>: ";
-        cin >> letter;
-    }
-
-    for (auto& c : letter)  // & zapamietuje modyfikacje w petli & - referencja
-        c = toupper(c);
-
-    return letter[0];
-
-}
-
-char WczytajWybor()
-{
-    char wybor;
-    wybor = WczytajZnak();
-
-    while (wybor != '1' && wybor != '2')
-    {
-        cout << endl << "Wybierz [1-2]: ";
-        cin >> wybor;
-    }
-
-    return wybor;
-
-}
-
-
-int main()
-{
-    int i;
-
-    string haslo; // = "wielka antarktyda";
-    string proba;
-    char literka;
+class Game {
+public:
     int n;
+    string haslo;
     int maska[100];
-    int suma = 0;
-    int zgadl = 0;
-    int sa_spolgloski = 0;
-    int kwota = 0;
-    char wybor;
-    srand(time(NULL));
-    string rezultat;
+    int kolejka;
+    int suma;
+    int sa_spolgloski;
+    int kwota;
     vector<string> hasla;
-    int j;
-
+    char wybor;
+    char literka;
+    string proba;
+    int zgadl;
     ifstream strum;
-    strum.open("dane.txt");
-
-    setlocale(LC_CTYPE, "Polish");
-
-    while (!strum.eof())
-    {
-        string s;
-        //strum >> s;
-        getline(strum, s);
-        //cout << s << endl;
-        hasla.push_back(s);
-
+    string rezultat;
+    Player gracze[3];
+    static const int Kolo[15];
+    void WczytajHasla() {
+        strum.open("dane.txt");
+        setlocale(LC_CTYPE, "Polish");
+        while (!strum.eof()) {
+            string s;
+            getline(strum, s);
+            hasla.push_back(s);
+        }
+        strum.close();
+        assert(hasla.size() > 0);
     }
-
-    strum.close();
-
-    
-
-
-   // for (i = 0; i < hasla.size(); i++)
-    //    cout << hasla[i] << endl;
-
-    
-
-    for(string item : hasla)
-        cout << item << endl;
-
-    
-
-    cout << endl << endl;
-    
-    assert(hasla.size() > 0);
-    j = rand() % hasla.size(); // losujemy j-te haslo z przedzialu 0 ... size - 1
-    //cout << hasla[j];
-
-    haslo = hasla[j];
-    n = haslo.size();
-
-    gracze[0].imie = "Bryanusz";
-    gracze[1].imie = "Jessica ";
-    gracze[2].imie = "Nepomucen";
-
-    for (i = 0; i < n; i++)
-    {
-        if (haslo[i] == ' ')
-            maska[i] = 0;
-        else
-            maska[i] = 1;
-    }
-
-
-
-
-    do
-    {
-       // cout << "\033[2J";
-
-        cout << "\033[47m" << "\033[31m";
-        for (i = 0; i < n; i++)
-        {
-
-            if (maska[i] == 1)
-                cout << ".";
-            else
-                cout << haslo[i];
-        }
-
-        cout << "\033[0m";
-        cout << endl;
-
-
-        sa_spolgloski = 0;
-        kwota = 0;
-
-        for(i=0; i<n; i++)
-            if ((!jestSamogloska(haslo[i])) && (maska[i]))
-            {
-                sa_spolgloski = 1;
-                break;
-            }
-
-        if (sa_spolgloski)
-            cout << " -- Spolgloski sa --" << endl;
-         
-
-        //cout << gracze[kolejka].imie << " " << endl;
-        textPlayers();
-
-        cout << "1. zgaduj haslo" << endl;
-        cout << "2. krecenie kolem" << endl;
-
-        // TODO tylko 1 i 2, nie mozna wprowadzac liter
-
-        wybor = WczytajWybor();
-
-        if (wybor == '1')
-        {
-            cout << "Podaj haslo" << endl;
-            getline(cin >> ws, proba);  // wczytanie z klawiatury string z uwzgl. whitespa
-            for (auto& c : proba)  // & zapamietuje modyfikacje w petli & - referencja
-                c = toupper(c);
-            if (haslo == proba) {
-                cout << endl << " !!!!!!!!!! =======   WYGRANA ========== !!!!!!!!!!!!!" << endl;
-                gracze[kolejka].portfel += gracze[kolejka].kasa;
-                break;
-            }
-            else
-            {
-                kolejka = (kolejka + 1) % 3;
-                suma = 1;
-                cout << endl << " !!!!!!!!!! =======   ZLE ========== !!!!!!!!!!!!!" << endl;
-                cout << endl << "=================================================" << endl;
-                continue;
-
-
-            }
-
-
-
-        }
-
-
-        rezultat = "";
-        i = rand() % 15;
-        if (Kolo[i] == 0)
-            rezultat = "Strata kolejki";
-        if (Kolo[i] == -1)
-            rezultat = "Bankrut";
-        if (rezultat != "")
-           cout << "\033[1;31m" << rezultat << "\033[0m" << endl;
-        else
-        {
-            cout << "\033[1;34m" << Kolo[i] << "\033[0m" << endl;
-            kwota = Kolo[i];
-        }
-
-        // Strata kolejki i bankrut
-        if ((Kolo[i] == 0) || (Kolo[i] == -1))
-        {
-            if (Kolo[i] == -1) // bankrut
-                gracze[kolejka].kasa = 0;
-            
-            kolejka = (kolejka + 1) % 3;
-            suma = 1;
-            cout << endl << "=================================================" << endl;
-
-            // TODO
-            // sleep for
-            // kupowanie samoglosek
-
-            continue;
-
-        }
-
-
-
-
-
-        cout << gracze[kolejka].imie <<  ": Podaj litere" << endl;
-
-
-
-        literka = WczytajZnak();
- 
-
-        zgadl = 0;
-
-        if (jestSamogloska(literka))
-            cout << "samogloska";
-        else
-            cout << "spolgloska";
-        cout << endl;
-
-        for (i = 0; i < n; i++)
-        {
-            if ((haslo[i] == literka) && (maska[i]==1)) {
+    void Inicjuj() {
+        srand(time(NULL));
+        WczytajHasla();
+        int j = rand() % hasla.size();
+        haslo = hasla[j];
+        n = haslo.size();
+        for (int i = 0; i < n; i++) {
+            if (haslo[i] == ' ')
                 maska[i] = 0;
-                zgadl++;
-            }
+            else
+                maska[i] = 1;
         }
-
-        if (zgadl)
-        {
-            cout << "OK";
-            gracze[kolejka].kasa += kwota * zgadl;
-
-            cout << endl << gracze[kolejka].imie << "\033[1;32m " << gracze[kolejka].kasa << "\033[0m";
-
-
-        }
-        else {
-            cout << "Zle!";
-            // strata kolejki
-            kolejka = (kolejka + 1) % 3;
-            cout << endl << "=================================================" << endl;
-            suma = 1;
-            continue;
-        }
-
-        
-
-
-        cout << endl;
-
-
-
+        gracze[0].imie = "Bryanusz";
+        gracze[1].imie = "Jessica";
+        gracze[2].imie = "Nepomucen";
+    }
+    void textPlayers() {
+        cout << "Gracze i stan kont:" << endl;
+        for (int i = 0; i < 3; i++)
+            cout << gracze[i].imie << " " << gracze[i].portfel << endl;
+    }
+    void Play() {
+        Inicjuj();
+        kolejka = 0;
         suma = 0;
+        while (1) {
+            cout << "\033[2J";
+            cout << "\033[47m" << "\033[31m";
+            for (int i = 0; i < n; i++) {
+                if (maska[i] == 1)
+                    cout << ".";
+                else
+                    cout << haslo[i];
+            }
+            cout << "\033[0m";
+            cout << endl;
+            sa_spolgloski = 0;
+            kwota = 0;
+            for (int i = 0; i < n; i++)
+                if ((!jestSamogloska(haslo[i])) && (maska[i])) {
+                    sa_spolgloski = 1;
+                    break;
+                }
+            if (sa_spolgloski)
+                cout << " -- Spolgloski sa --" << endl;
+            textPlayers();
+            cout << "1. zgaduj haslo" << endl;
+            cout << "2. krecenie kolem" << endl;
+            wybor = WczytajWybor();
+            if (wybor == '1') {
+                cout << "Podaj haslo" << endl;
+                getline(cin >> ws, proba);
+                for (auto& c : proba)
+                    c = toupper(c);
+                if (haslo == proba) {
+                    cout <<
 
-        for (i = 0; i < n; i++)
-        {
-            suma += maska[i];
-        }
-
-       
-
-    } while (suma);
-    cout << "BRAWO!";
-    exit(0);
-
-    textPlayers();
-    Beep(523, 500);
+"Wygrales! Haslo to: " << haslo << endl;
+gracze[kolejka].portfel += suma;
+textPlayers();
+break;
 }
+else {
+cout << "Nieprawidlowe haslo" << endl;
+gracze[kolejka].portfel -= 500;
+textPlayers();
+}
+}
+else {
+cout << "Kręcę kołem" << endl;
+int los = rand() % 15;
+cout << "Wylosowana wartość: " << Kolo[los] << endl;
+if (Kolo[los] == 0) {
+cout << "Bankrut! Tracisz swoją sumę" << endl;
+gracze[kolejka].portfel = 0;
+suma = 0;
+kolejka = (kolejka + 1) % 3;
+}
+else if (Kolo[los] == -1) {
+cout << "Porażka! Kolejka przepada." << endl;
+kolejka = (kolejka + 1) % 3;
+}
+else {
+cout << "Wybierz literę" << endl;
+literka = WczytajLiterke();
+int ile = 0;
+for (int i = 0; i < n; i++) {
+if (haslo[i] == literka) {
+maska[i] = 0;
+ile++;
+}
+}
+if (ile == 0) {
+cout << "Nie ma takiej litery" << endl;
+kolejka = (kolejka + 1) % 3;
+}
+else {
+kwota = Kolo[los] * ile;
+suma += kwota;
+cout << "Wygrałeś: " << kwota << endl;
+cout << "Suma: " << suma << endl;
+zgadl = 0;
+for (int i = 0; i < n; i++)
+if (maska[i] == 1) {
+zgadl = 1;
+break;
+}
+if (zgadl == 0) {
+cout << "Wygrał " << gracze[kolejka].imie << endl;
+gracze[kolejka].portfel += suma;
+textPlayers();
+break;
+}
+else {
+kolejka = (kolejka + 1) % 3;
+}
+}
+}
+}
+}
+}
+char WczytajWybor() {
+char wybor;
+cout << "Twój wybór: ";
+while (1) {
+cin >> wybor;
+if ((wybor == '1') || (wybor == '2'))
+return wybor;
+cout << "Nieprawidłowy wybór. Spróbuj ponownie." << endl;
+}
+}
+char WczytajLiterke() {
+char literka;
+cout << "Podaj literę: ";
+while (1) {
+cin >> literka;
+if (isalpha(literka))
+return toupper(literka);
+cout << "Nieprawidłowa litera. Spróbuj ponownie." << endl;
+}
+}
+};
