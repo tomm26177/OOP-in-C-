23ce7c0e7949c633c274e8f88ed50eb8bb41ddcc
Tomasz Kaminski <tomasz77ka@gmail.com>
2023-05-01 20:41:21 +0200

    digitalemillis

--- zadanie_3_oop/main.cpp
+++ zadanie_3_oop/main.cpp
@@ -9,35 +9,20 @@
 
 
 
+int Tree::counter = 0;
+int Shape::counter = 0;
+
+int main() {
+
+    Triangle t(3, '+', "blue");
+     Border b(3, 9, '-', "white");
+    Rectangle r(7, 9, '|', "green");
+
+    t.Print();
+    r.Print();
+    b.Print();
 
 
-/* Do projektu Forest dodajemy klasy potomne po Shape
-
-Triangle (choinka)
-Trojkat prostokatny
-Rectangle
-Border (jako wnuk, Shape, dziecko rectangle). Dodatkowy argument w konstruktorze.
-
-Forest trees -> Tree* Shape*
-
-Docelowo na Forest narysowac domek, okno jako ramka, dach , itd. */
-
-
-
-
-
-
-
-int main()
-{
-
-
-
-   return 0;
+    return 0;
 }
-
-
-
-
-
 
