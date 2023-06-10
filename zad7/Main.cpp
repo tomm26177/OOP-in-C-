a9e5f375603378ac8d50c663047dbb6726e60625
48510 <tomasz77ka@gmail.com>
2023-06-10 15:01:22 +0200

    i pyyyk comit

--- zad7/Main.cpp
+++ zad7/Main.cpp
@@ -1,198 +1,238 @@
 #include <iostream>
 #include <vector>
-#include <random>
-#include <fstream>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
 
-enum class CellState { Empty, Ship, Miss, Hit };
-
-class Board {
-protected:
-    int size_;
-    std::vector<std::vector<CellState>> cells_;
-
+// Klasa reprezentująca pole na planszy
+class Pole {
 public:
-    Board(int size) : size_(size) {
-        cells_.resize(size_, std::vector<CellState>(size_, CellState::Empty));
+    enum class Stan { Puste, Statek, Trafiony, Pudlo };
+
+    Pole() : stan(Stan::Puste) {}
+
+    void ustawStan(Stan nowyStan) {
+        stan = nowyStan;
     }
 
-    int getSize() const {
-        return size_;
+    Stan pobierzStan() const {
+        return stan;
     }
 
-    CellState getCellState(int row, int col) const {
-        return cells_[row][col];
+private:
+    Stan stan;
+};
+
+// Klasa reprezentująca planszę
+class Plansza {
+public:
+    Plansza(int rozmiar) : rozmiar(rozmiar) {
+        plansza.resize(rozmiar, std::vector<Pole>(rozmiar));
+    }
+    int pobierzRozmiar(){
+        return rozmiar;
+
     }
 
-    void setCellState(int row, int col, CellState state) {
-        cells_[row][col] = state;
-    }
-
-    void display(bool showShips) const {
-        std::cout << "   ";
-        for (int col = 0; col < size_; ++col) {
-            std::cout << col << " ";
-        }
-        std::cout << std::endl;
-
-        for (int row = 0; row < size_; ++row) {
-            std::cout << row << " ";
-            for (int col = 0; col < size_; ++col) {
-                char symbol;
-                switch (cells_[row][col]) {
-                    case CellState::Empty:
-                        symbol = showShips ? '-' : ' ';
+    void wyswietl() const {
+        for (const auto& wiersz : plansza) {
+            for (const auto& pole : wiersz) {
+                switch (pole.pobierzStan()) {
+                    case Pole::Stan::Puste:
+                        std::cout << "- ";
                         break;
-                    case CellState::Ship:
-                        symbol = showShips ? 'S' : ' ';
+                    case Pole::Stan::Statek:
+                        std::cout << "S ";
                         break;
-                    case CellState::Miss:
-                        symbol = 'M';
+                    case Pole::Stan::Trafiony:
+                        std::cout << "X ";
                         break;
-                    case CellState::Hit:
-                        symbol = 'X';
+                    case Pole::Stan::Pudlo:
+                        std::cout << "O ";
                         break;
                 }
-                std::cout << symbol << " ";
             }
             std::cout << std::endl;
         }
     }
-};
 
-class PlayerBoard : public Board {
-public:
-    PlayerBoard(int size) : Board(size) {}
 
-    void placeShipsRandomly() {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<int> dis(0, size_ - 1);
-
-        int shipCount = 0;
-        while (shipCount < 5) {
-            int row = dis(gen);
-            int col = dis(gen);
-
-            if (cells_[row][col] == CellState::Empty) {
-                cells_[row][col] = CellState::Ship;
-                shipCount++;
+    void wyswietlBezStatkow() const {
+        for (const auto& wiersz : plansza) {
+            for (const auto& pole : wiersz) {
+                switch (pole.pobierzStan()) {
+                    case Pole::Stan::Puste:
+                        std::cout << "- ";
+                        break;
+                    case Pole::Stan::Statek:
+                        std::cout << "- ";
+                        break;
+                    case Pole::Stan::Trafiony:
+                        std::cout << "X ";
+                        break;
+                    case Pole::Stan::Pudlo:
+                        std::cout << "O ";
+                        break;
+                }
             }
-        }
-    }
-};
-
-class ComputerBoard : public Board {
-public:
-    ComputerBoard(int size) : Board(size) {}
-
-    void placeShipsRandomly() {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<int> dis(0, size_ - 1);
-
-        int shipCount = 0;
-        while (shipCount < 5) {
-            int row = dis(gen);
-            int col = dis(gen);
-
-            if (cells_[row][col] == CellState::Empty) {
-                cells_[row][col] = CellState::Ship;
-                shipCount++;
-            }
+            std::cout << std::endl;
         }
     }
 
-    std::pair<int, int> generateRandomShot() {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<int> dis(0, size_ - 1);
-
-        int row = dis(gen);
-        int col = dis(gen);
-
-        return std::make_pair(row, col);
+    Pole& operator()(int wiersz, int kolumna) {
+        return plansza[wiersz][kolumna];
     }
-};
 
-class Game {
 private:
-    PlayerBoard playerBoard_;
-    ComputerBoard computerBoard_;
+    std::vector<std::vector<Pole>> plansza;
+    int rozmiar;
+};
 
+// Klasa reprezentująca grę
+class Gra {
 public:
-    Game(int boardSize) : playerBoard_(boardSize), computerBoard_(boardSize) {}
+    Gra(int rozmiarPlanszy) : planszaGracza(rozmiarPlanszy), planszaPrzeciwnika(rozmiarPlanszy) {}
 
-    void start() {
-        playerBoard_.placeShipsRandomly();
-        computerBoard_.placeShipsRandomly();
+    void rozpocznijGre() {
+        rozmiescStatki(&planszaGracza);
+        rozmiescStatki(&planszaPrzeciwnika);
 
-        while (true) {
-            std::cout << "Player's turn:" << std::endl;
-            playerTurn();
-            if (checkGameOver()) {
-                std::cout << "Congratulations! You have won the game!" << std::endl;
-                break;
+        while (!(czyGraSkonczona(&planszaGracza)&& czyGraSkonczona(&planszaPrzeciwnika))) {
+            ruchGracza();
+            ruchPrzeciwnika();
+        }
+
+        wyswietlWynik();
+    }
+
+private:
+    Plansza planszaGracza;
+    Plansza planszaPrzeciwnika;
+
+    void rozmiescStatki(Plansza* planszaGracza) {
+        // Inicjalizacja generatora liczb losowych
+        srand(time(nullptr));
+
+        // Tablica z długościami statków
+        std::vector<int> dlugosciStatkow = {5, 4, 3, 3, 2};
+
+        for (int dlugosc : dlugosciStatkow) {
+            bool ustawiony = false;
+
+            // Losowe ustawianie statków, dopóki nie zostanie ustawiony poprawnie
+            while (!ustawiony) {
+                int wiersz = rand() % planszaGracza->pobierzRozmiar();
+                int kolumna = rand() % planszaGracza->pobierzRozmiar();
+
+                // Losowe ustawienie pionowe lub poziome
+                bool pionowo = rand() % 2 == 0;
+
+                if (pionowo) {
+                    // Sprawdzanie czy można ustawić statek pionowo
+                    if (wiersz + dlugosc <= planszaGracza->pobierzRozmiar()) {
+                        bool moznaUstawic = true;
+
+                        // Sprawdzanie czy pola nie są już zajęte
+                        for (int i = wiersz; i < wiersz + dlugosc; ++i) {
+                            if (planszaGracza->operator()(i, kolumna).pobierzStan() != Pole::Stan::Puste) {
+                                moznaUstawic = false;
+                                break;
+                            }
+                        }
+
+                        // Ustawianie statku pionowo
+                        if (moznaUstawic) {
+                            for (int i = wiersz; i < wiersz + dlugosc; ++i) {
+                                planszaGracza->operator()(i, kolumna).ustawStan(Pole::Stan::Statek);
+                            }
+                            ustawiony = true;
+                        }
+                    }
+                } else {
+                    // Sprawdzanie czy można ustawić statek poziomo
+                    if (kolumna + dlugosc <= planszaGracza->pobierzRozmiar()) {
+                        bool moznaUstawic = true;
+
+                        // Sprawdzanie czy pola nie są już zajęte
+                        for (int j = kolumna; j < kolumna + dlugosc; ++j) {
+                            if (planszaGracza->operator()(wiersz, j).pobierzStan() != Pole::Stan::Puste) {
+                                moznaUstawic = false;
+                                break;
+                            }
+                        }
+
+                        // Ustawianie statku poziomo
+                        if (moznaUstawic) {
+                            for (int j = kolumna; j < kolumna + dlugosc; ++j) {
+                                planszaGracza->operator()(wiersz, j).ustawStan(Pole::Stan::Statek);
+                            }
+                            ustawiony = true;
+                        }
+                    }
+                }
             }
-
-            std::cout << "Computer's turn:" << std::endl;
-            computerTurn();
-            if (checkGameOver()) {
-                std::cout << "Game over! The computer has won." << std::endl;
-                break;
-            }
         }
     }
 
-    void playerTurn() {
-        int row, col;
-        do {
-            std::cout << "Enter row and column for your shot: ";
-            std::cin >> row >> col;
-        } while (!isValidShot(row, col));
+    void ruchGracza() {
 
-        processShot(row, col, computerBoard_);
-        playerBoard_.display(false);
-    }
+        std::cout << "plansza oponenta" << "\n";
 
-    void computerTurn() {
-        std::pair<int, int> shot = computerBoard_.generateRandomShot();
-        int row = shot.first;
-        int col = shot.second;
+        planszaPrzeciwnika.wyswietlBezStatkow();
+        std::cout<< "------------------------------------------------------"<<"\n";
 
-        processShot(row, col, playerBoard_);
-        computerBoard_.display(true);
-    }
+        std::cout << "Twoja plansza:" << std::endl;
+        planszaGracza.wyswietl();
 
-    bool isValidShot(int row, int col) const {
-        if (row < 0 || row >= playerBoard_.getSize() || col < 0 || col >= playerBoard_.getSize()) {
-            std::cout << "Invalid shot! Please enter valid coordinates." << std::endl;
-            return false;
+
+
+
+        int wiersz, kolumna;
+        std::cout << "Podaj wspolrzedne strzalu (wiersz, kolumna): ";
+        std::cin >> wiersz >> kolumna;
+
+
+        if (wiersz < 0 || wiersz >= planszaPrzeciwnika.pobierzRozmiar() ||
+            kolumna < 0 || kolumna >= planszaPrzeciwnika.pobierzRozmiar()) {
+            std::cout << "Niepoprawne wspolrzedne strzalu!" << std::endl;
+            return;
         }
 
-        CellState cellState = playerBoard_.getCellState(row, col);
-        if (cellState == CellState::Miss || cellState == CellState::Hit) {
-            std::cout << "You have already shot at this location! Please choose another one." << std::endl;
-            return false;
-        }
+        Pole& polePrzeciwnika = planszaPrzeciwnika(wiersz, kolumna);
 
-        return true;
-    }
-
-    void processShot(int row, int col, Board& board) {
-        CellState cellState = board.getCellState(row, col);
-        if (cellState == CellState::Ship) {
-            std::cout << "Hit!" << std::endl;
-            board.setCellState(row, col, CellState::Hit);
-        } else {
-            std::cout << "Miss!" << std::endl;
-            board.setCellState(row, col, CellState::Miss);
+        if (polePrzeciwnika.pobierzStan() == Pole::Stan::Statek) {
+            std::cout << "TRAFIONY!" << std::endl;
+            polePrzeciwnika.ustawStan(Pole::Stan::Trafiony);
+        } else if (polePrzeciwnika.pobierzStan() == Pole::Stan::Puste) {
+            std::cout << "PUDLO!" << std::endl;
+            polePrzeciwnika.ustawStan(Pole::Stan::Pudlo);
         }
     }
 
-    bool checkGameOver() const {
-        for (int row = 0; row < playerBoard_.getSize(); ++row) {
-            for (int col = 0; col < playerBoard_.getSize(); ++col) {
-                if (playerBoard_.getCellState(row, col) == CellState::Ship) {
+    void ruchPrzeciwnika() {
+        int wiersz = rand() % planszaGracza.pobierzRozmiar();
+        int kolumna = rand() % planszaGracza.pobierzRozmiar();
+
+        Pole& poleGracza = planszaGracza(wiersz, kolumna);
+
+        if (poleGracza.pobierzStan() == Pole::Stan::Statek) {
+            std::cout << "Przeciwnik trafia Twoj statek!" << std::endl;
+            poleGracza.ustawStan(Pole::Stan::Trafiony);
+        } else if (poleGracza.pobierzStan() == Pole::Stan::Puste) {
+            std::cout << "Przeciwnik pudluje!" << std::endl;
+            poleGracza.ustawStan(Pole::Stan::Pudlo);
+        }
+    }
+
+    bool czyGraSkonczona(Plansza* p) {
+        // Sprawdzenie, czy gra się skończyła (czy wszystkie statki zostały zatopione)
+        // Można zaimplementować własną logikę zakończenia gry.
+        // W przypadku, gdy wszystkie statki zostaną trafione, można zwrócić true.
+
+        // Przykład sprawdzenia, czy wszystkie statki zostały zatopione:
+        for (int i = 0; i < p->pobierzRozmiar(); ++i) {
+            for (int j = 0; j < p->pobierzRozmiar(); ++j) {
+                if (p->operator()(i, j).pobierzStan() == Pole::Stan::Statek) {
                     return false;
                 }
             }
@@ -200,15 +240,25 @@ public:
 
         return true;
     }
+
+    void wyswietlWynik() {
+        std::cout << "Koniec gry!" << std::endl;
+        std::cout << "Twoja plansza:" << std::endl;
+        planszaGracza.wyswietl();
+        std::cout << "Plansza przeciwnika:" << std::endl;
+        planszaPrzeciwnika.wyswietl();
+    }
 };
 
 int main() {
-    int boardSize;
-    std::cout << "Enter the size of the board: ";
-    std::cin >> boardSize;
+    // Inicjalizacja generatora liczb losowych
+    srand(time(nullptr));
 
-    Game game(boardSize);
-    game.start();
+    // Utworzenie gry
+    Gra gra(10);
+
+    // Rozpoczęcie gry
+    gra.rozpocznijGre();
 
     return 0;
 }
